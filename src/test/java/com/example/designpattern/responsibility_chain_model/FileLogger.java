2ff0333825754075d1b7d87814866bf5f5e8c4ed
yulin <yulin.li@langyangtech.com>
2021-02-01 11:11:17 +0800

    init

---
+++ src/test/java/com/example/designpattern/responsibility_chain_model/FileLogger.java
@@ -0,0 +1,13 @@
+package com.example.designpattern.responsibility_chain_model;
+
+public class FileLogger extends AbstractLogger {
+
+    public FileLogger(int level){
+        this.level = level;
+    }
+
+    @Override
+    protected void write(String message) {
+        System.out.println("File::Logger: " + message);
+    }
+}
---
+++ src/test/java/com/example/designpattern/responsibility_chain_model/chain_pattern_demo/ChainPatternDemo.java
@@ -0,0 +1,36 @@
+package com.example.designpattern.responsibility_chain_model.chain_pattern_demo;
+
+import com.example.designpattern.responsibility_chain_model.AbstractLogger;
+import com.example.designpattern.responsibility_chain_model.ConsoleLogger;
+import com.example.designpattern.responsibility_chain_model.ErrorLogger;
+import com.example.designpattern.responsibility_chain_model.FileLogger;
+
+/**
+ * 责任链模式测试
+ */
+public class ChainPatternDemo {
+
+    private static AbstractLogger getChainOfLoggers(){
+
+        AbstractLogger errorLogger = new ErrorLogger(AbstractLogger.ERROR);
+        AbstractLogger fileLogger = new FileLogger(AbstractLogger.DEBUG);
+        AbstractLogger consoleLogger = new ConsoleLogger(AbstractLogger.INFO);
+
+        errorLogger.setNextLogger(fileLogger);
+        fileLogger.setNextLogger(consoleLogger);
+
+        return errorLogger;
+    }
+
+    public static void main(String[] args) {
+        AbstractLogger loggerChain = getChainOfLoggers();
+
+        loggerChain.logMessage(AbstractLogger.INFO, "This is an information.");
+
+        loggerChain.logMessage(AbstractLogger.DEBUG,
+                "This is a debug level information.");
+
+        loggerChain.logMessage(AbstractLogger.ERROR,
+                "This is an error information.");
+    }
+}
